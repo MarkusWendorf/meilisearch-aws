8a7f2c26c0f950fe09d7bc08a5fd13580156567d
Markus Wendorf <markuswendorf@web.de>
2022-03-12 15:44:19 +0100

    initial

--- lib/test-meili-stack.ts
+++ lib/test-meili-stack.ts
@@ -1,16 +1,26 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 import { Construct } from 'constructs';
+import { HttpIntegration, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { cwd } from 'process';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestMeiliStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const code = lambda.DockerImageCode.fromImageAsset(cwd() + "/src");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestMeiliQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myLambda = new lambda.DockerImageFunction(this, "Lambda", {
+      code,
+    });
+
+    const api = new apigw.RestApi(this, "RestAPI", {});
+    const resource = api.root.addResource("{proxy+}", {
+      defaultIntegration: new LambdaIntegration(myLambda),
+    });
+
+    resource.addMethod("ANY");
   }
 }
